cfc0663b3bd7fda79291d5cdb0e319e9d7d6e274
dm1tro69 <dimolg22@gmail.com>
2021-06-07 18:33:36 +0300

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://api.themoviedb.org/3";
 
-export const API_KEY_3 = "3f4ca4f3a9750da53450646ced312397";
+export const API_KEY_3 = "3ba4374a07cce346680195c8ebba6a27";
 
 export const API_KEY_4 =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjRjYTRmM2E5NzUwZGE1MzQ1MDY0NmNlZDMxMjM5NyIsInN1YiI6IjVhYzlmNWRkOTI1MTQxNjJhZTA1Njk0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fc4f9DVB6pFWh6hIjYe0NCC4pImdmNzDIfi_3Nb3tC4";
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmE0Mzc0YTA3Y2NlMzQ2NjgwMTk1YzhlYmJhNmEyNyIsInN1YiI6IjYwYmQyNzExZjVjODI0MDAyN2ZhYzkxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R5x7_q0hPwe7UyKxJLFtoN-mB0n9gnw6Yhcikj6V3xo";
